2e515ea03ebd248c287faa0f36ad7bcfad3cc135
Zizu <33559590+Zimzer@users.noreply.github.com>
2025-09-16 22:21:09 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,6 +72,7 @@ const translations = {
     cmd_greet_desc: "Manages the server welcome message",
     cmd_logs_desc: "Set up or disable a moderation log feed",
     cmd_movie_desc: "Display movie/series information",
+    cmd_top_desc: "Display server rankings from a given category",
     // Community Section
     community_title: "Join Our Community",
     community_subtitle: "Get help, share feedback, and connect with other server owners",
@@ -171,6 +172,7 @@ const translations = {
     cmd_greet_desc: "Zarządza komunikatem powitalnym serwera",
     cmd_logs_desc: "Ustaw lub wyłącz kanał logów moderacyjnych",
     cmd_movie_desc: "Wyświetl informacje o filmie/serialu",
+    cmd_top_desc: "Wyświetl ranking serwera z danej kategorii",
     // Community Section
     community_title: "Dołącz do Naszej Społeczności",
     community_subtitle: "Uzyskaj pomoc, podziel się opinią i połącz się z innymi właścicielami serwerów",
@@ -618,6 +620,10 @@ function generateCommandGroups() {
         <div class="command-item">
           <div class="command-name"><i class="fa-solid fa-slash" style="transform: scaleX(-0.5); margin-right: -5px;"></i>movieinfo</div>
           <div class="command-description" data-translate="cmd_movie_desc">Display movie/series information</div>
+          </div>
+        <div class="command-item">
+          <div class="command-name"><i class="fa-solid fa-slash" style="transform: scaleX(-0.5); margin-right: -5px;"></i>top</div>
+          <div class="command-description" data-translate="cmd_top_desc">Display movie/series information</div>
           </div>
         </div>
     </div>
